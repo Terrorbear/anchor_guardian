3ec3bc00bc081c973509b7119b3f61e75becb8b0
johnny <johnjchang@gmail.com>
2022-05-03 18:25:26 -0500

    bugfix - state item

--- contracts/cw20/src/state.rs
+++ contracts/cw20/src/state.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 use cw_storage_plus::{Item, Map};
 
 pub const BORROWERS: Map<Addr, Borrower> = Map::new("borrowers");
-pub const STATE: Item<State> = Item::new("\u{0}\u{6}config");
+pub const STATE: Item<State> = Item::new("\u{0}\u{5}state");
 pub const CONFIG: Item<Config> = Item::new("\u{0}\u{6}config");
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
